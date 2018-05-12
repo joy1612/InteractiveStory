b376f300e22502867c7d3a0e5106aa53f7108f17
Jay Zalawadia <jayzalawadia@jays-mbp-2.fios-router.home>
2018-05-12 17:21:51 -0400

    Initial Commit

---
+++ InteractiveStory/Audio.swift
@@ -0,0 +1,76 @@
+//
+//  Audio.swift
+//  InteractiveStory
+//
+//  Created by Screencast on 1/17/17.
+//  Copyright © 2017 Treehouse Island. All rights reserved.
+//
+
+import Foundation
+import AudioToolbox
+
+extension Story {
+    var soundEffectName: String {
+        switch self {
+        case .droid, .home: return "HappyEnding"
+        case .monster: return "Ominous"
+        default: return "PageTurn"
+        }
+    }
+    
+    var soundEffectURL: URL {
+        let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+        return URL(fileURLWithPath: path)
+    }
+}
+
+class SoundEffectsPlayer {
+    var sound: SystemSoundID = 0
+    
+    func playSound(for story: Story) {
+        let soundURL = story.soundEffectURL as CFURL
+        AudioServicesCreateSystemSoundID(soundURL, &sound)
+        AudioServicesPlaySystemSound(sound)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
